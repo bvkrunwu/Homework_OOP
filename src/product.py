6012c9657e42f65061441c828392e271ff9663ac
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2025-12-10 02:47:23 +0300

    Add new functionality to product module

--- src/product.py
+++ src/product.py
@@ -56,3 +56,30 @@ class Product:
                 return
 
         self.__price = value
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+    def __add__(self, other):
+        if type(other) is not Smartphone:
+            raise TypeError("Нельзя складывать товары разных категорий")
+        return super().__add__(other)
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __add__(self, other):
+        if type(other) is not LawnGrass:
+            raise TypeError("Нельзя складывать товары разных категорий")
+        return super().__add__(other)
